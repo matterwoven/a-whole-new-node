e6c78ddcb0d88990afbf0f129814552545054843
Matthew Vargas <145516753+matterwoven@users.noreply.github.com>
2026-09-24 11:05:18 -0700

    Break time

--- src/Traversals.java
+++ src/Traversals.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Traversals {
   public static void main(String[] args) {
@@ -39,5 +42,25 @@ public class Traversals {
     child3_1.children = new ArrayList<>();
     child3_1.children.add(child3_1_1);
     child3_1.children.add(child3_1_2);
+
+    Map<Integer, List<Integer>> tree = new HashMap<>();
+
+    tree.put(88, List.of(17, 58, 33));
+    //tree.put(88, new ArrayList<>(List.of(17, 58, 33)));
+
+    tree.put(17, List.of(5, 99));
+    tree.put(58, List.of(73));
+    tree.put(33, List.of(24, 61, 12));
+    tree.put(24, List.of(83, 6));
+  }
+
+  public static <T> void preorder(Node<?> node){
+    if(node == null) return;
+    
+    System.out.println(node.value);
+
+    for(Node<?> child : node.children){
+      preorder(child);
+    }
   }
 }
